f911c1f687a3a2d5c295c3bd090ac4534753bf8b
Paul Novack <paul.novack@gmail.com>
2023-06-04 17:19:28 -0500

    WIP context fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { ChakraProvider, Box, HStack } from '@chakra-ui/react'
 import Home from './pages/Home'
@@ -8,39 +8,68 @@ import Boxes from './pages/Boxes'
 import Boxe from './pages/Boxe'
 import Items from './pages/Items'
 import Item from './pages/Item'
+import UserProvider, { UserContext } from './context/UserContext'
 
+function UserDetails() {
+
+  const {user} = useContext(UserContext)
+  return (
+    <>
+      <p>User Name:</p>
+      <p>User Email: </p>
+    </>
+  )
+}
+function AnotherComponent() {
+
+  const {user, setUser} = useContext(UserContext)
+  useEffect(() => {
+    setUser({
+      name: "Paul Novack",
+      email: 'paul.novack@gmail.com'
+    })
+  })
+  return (
+    <>
+      <p>Some Other Component</p>
+    </>
+  )
+}
 
 function App() {
-
   return (
     <ChakraProvider>
-      <BrowserRouter>
-        <div className="app">
-          <HStack>
-            <Box>
-              <Link to={'/'}>Home</Link>
-            </Box>
-            <Box>
-              <Link to={'/login'}>Login</Link>
-            </Box>
-            <Box>
-              <Link to={'/logout'}>Logout</Link>
-            </Box>
-            <Box>
-              <Link to={'/boxes'}>Boxes</Link>
-            </Box>
-          </HStack>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/boxes" element={<Boxes />} />
-            <Route path="/box" element={<Boxe />} />
-            <Route path="/items" element={<Items />} />
-            <Route path="/item" element={<Item />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <UserProvider>
+        <UserDetails />.
+        <AnotherComponent />
+        <BrowserRouter>
+          <div className="app">
+            <HStack>
+              <Box>
+                <Link to={'/'}>Home</Link>
+              </Box>
+              <Box>
+                <Link to={'/login'}>Login</Link>
+              </Box>
+              <Box>
+                <Link to={'/logout'}>Logout</Link>
+              </Box>
+              <Box>
+                <Link to={'/boxes'}>Boxes</Link>
+              </Box>
+            </HStack>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/boxes" element={<Boxes />} />
+              <Route path="/box" element={<Boxe />} />
+              <Route path="/items" element={<Items />} />
+              <Route path="/item" element={<Item />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </UserProvider>
     </ChakraProvider>
   )
 }
