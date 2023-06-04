b7ee75871d9867b5a09d564de803fe82acd39fea
Paul Novack <paul.novack@gmail.com>
2023-06-04 09:27:27 -0500

    adds chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Logout from './pages/Logout'
@@ -10,45 +11,46 @@ import Item from './pages/Item'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
-        <nav>
-          <ul>
-            <li>
-              <Link to={'/'}>Home</Link>
-            </li>
-            <li>
-              <Link to={'/login'}>Login</Link>
-            </li>
-            <li>
-              <Link to={'/logout'}>Logout</Link>
-            </li>
-            <li>
-              <Link to={'/boxes'}>Boxes</Link>
-            </li>
-            <li>
-              <Link to={'/box'}>Box</Link>
-            </li>
-            <li>
-              <Link to={'/items'}>Items</Link>
-            </li>
-            <li>
-              <Link to={'/item'}>Item</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/boxes" element={<Boxes />} />
-          <Route path="/box" element={<Box />} />
-          <Route path="/items" element={<Items />} />
-          <Route path="/item" element={<Item />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <div className="app">
+          <nav>
+            <ul>
+              <li>
+                <Link to={'/'}>Home</Link>
+              </li>
+              <li>
+                <Link to={'/login'}>Login</Link>
+              </li>
+              <li>
+                <Link to={'/logout'}>Logout</Link>
+              </li>
+              <li>
+                <Link to={'/boxes'}>Boxes</Link>
+              </li>
+              <li>
+                <Link to={'/box'}>Box</Link>
+              </li>
+              <li>
+                <Link to={'/items'}>Items</Link>
+              </li>
+              <li>
+                <Link to={'/item'}>Item</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/boxes" element={<Boxes />} />
+            <Route path="/box" element={<Box />} />
+            <Route path="/items" element={<Items />} />
+            <Route path="/item" element={<Item />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </ChakraProvider>
   )
 }
 
