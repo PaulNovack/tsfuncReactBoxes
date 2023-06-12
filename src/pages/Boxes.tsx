e52703fa5561b6e9d290d71457e79f16f6e26576
Paul Novack <paul.novack@gmail.com>
2023-06-12 06:57:14 -0500

    wrap browser and mobile view

--- src/pages/Boxes.tsx
+++ src/pages/Boxes.tsx
@@ -2,22 +2,37 @@ import React, { useContext } from 'react'
 import { BoxIfc, UserContext } from '../context/UserContext'
 import Boxe from '../components/Boxe'
 import { Grid, GridItem } from '@chakra-ui/react'
+import { BrowserView, MobileView } from 'react-device-detect'
 
 function Boxes() {
   const { user } = useContext(UserContext)
   console.log('User in Boxes: ', user.BoxArr)
   return (
     <div className="app">
-      <strong>List of Boxes</strong>
-      {user.BoxArr ? (
-        user.BoxArr.map((box: BoxIfc) => (
-          <>
-            <Boxe box={box} />
-          </>
-        ))
-      ) : (
-        <></>
-      )}
+      <MobileView>
+        <strong>List of Boxes</strong>
+        {user.BoxArr ? (
+          user.BoxArr.map((box: BoxIfc) => (
+            <>
+              <Boxe box={box} />
+            </>
+          ))
+        ) : (
+          <></>
+        )}
+      </MobileView>
+      <BrowserView>
+        <strong>List of Boxes</strong>
+        {user.BoxArr ? (
+          user.BoxArr.map((box: BoxIfc) => (
+            <>
+              <Boxe box={box} />
+            </>
+          ))
+        ) : (
+          <></>
+        )}
+      </BrowserView>
     </div>
   )
 }
--- src/pages/Items.tsx
+++ src/pages/Items.tsx
@@ -1,20 +1,28 @@
 import React, { useContext } from 'react'
 import { ItemIfc, UserContext } from '../context/UserContext'
 import Item from '../components/Item'
+import { BrowserView, MobileView } from 'react-device-detect'
 
 function Items() {
   const { user } = useContext(UserContext)
   console.log('User in Boxes: ', user)
   return (
     <div className="app">
-      <h1>Boxes</h1>
-      <ul>
+      <h1>Items</h1>
+      <MobileView>
         {user.BoxArr && user.BoxArr[0].itemArr ? (
           user.BoxArr[0].itemArr.map((item: ItemIfc) => <Item item={item} />)
         ) : (
           <></>
         )}
-      </ul>
+      </MobileView>
+      <BrowserView>
+        {user.BoxArr && user.BoxArr[0].itemArr ? (
+          user.BoxArr[0].itemArr.map((item: ItemIfc) => <Item item={item} />)
+        ) : (
+          <></>
+        )}
+      </BrowserView>
     </div>
   )
 }
